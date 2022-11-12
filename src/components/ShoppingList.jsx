018033231caddcbf33bcd265203dc40a12314556
Stellamaris Nwafor <wendumaris@gmail.com>
2022-11-12 23:32:21 +0100

    fixed form validation

--- src/components/ShoppingList.jsx
+++ src/components/ShoppingList.jsx
@@ -62,7 +62,27 @@ const ShoppingList = () => {
         if ((name != '') && (+price > 0)) {
             setShoppingList((prev) => [...prev, { name: name, price: price, isChecked: false }]);
         }
-        updateTotalPrice()
+        updateTotalPrice();
+    }
+
+    const updateHandler = (e) => {
+        e.preventDefault();
+        if ((name != '') && (+price > 0)) {
+            const updateListCopy = [...shoppingList];
+            updateListCopy[currentId].name = name;
+            updateListCopy[currentId].price = price;
+            setShoppingList(updateListCopy);
+            setName('');
+            setPrice(0);
+            setIsEdit(false);
+        }
+    }
+
+    const editIconHandler = (id) => {
+        setIsEdit(true);
+        setCurrentId(id);
+        setName(shoppingList[id].name);
+        setPrice(shoppingList[id].price);
     }
 
     const checkboxHandler = (e, id) => {
@@ -75,24 +95,6 @@ const ShoppingList = () => {
         // console.log(shoppingList);
     }
 
-    const updateHandler = (e) => {
-        e.preventDefault();
-        const updateListCopy = [...shoppingList];
-        updateListCopy[currentId].name = name;
-        updateListCopy[currentId].price = price;
-        setShoppingList(updateListCopy);
-        setName('');
-        setPrice(0);
-        setIsEdit(false);
-    }
-
-    const editIconHandler = (id) => {
-        setIsEdit(true);
-        setCurrentId(id);
-        setName(shoppingList[id].name);
-        setPrice(shoppingList[id].price);
-    }
-
     const deleteHandler = (id) => {
         const newShoppingList = shoppingList.filter(item => shoppingList.indexOf(item) != id);
         setShoppingList(newShoppingList);
@@ -100,11 +102,11 @@ const ShoppingList = () => {
 
 
     return (
-        <div className='md:container sm:p-0 bg-white mt-4 text-black py-5 px-8 h-auto md:flex justify-between box-border'>
+        <div className='sm:p-0 bg-white mt-4 text-black py-5 md:px-8 h-auto md:flex justify-between box-border'>
             <div className='p-4'>
-                <h1 className='text-4xl text-blue-600 font-bold '>Shopping App 🛒</h1>
-                <div className='md:w-96 w-full max-W-xs mt-4'>
-                    <form className='shadow-md rounded px-8 pt-6 pb-8 mb-4' onSubmit={isEdit ? updateHandler : submitHandler}>
+                <h1 className='md:text-4xl text-blue-600 font-bold text-center text-2xl '>Shopping App 🛒</h1>
+                <div className='md:w-96 w-full md:max-W-xs mt-4'>
+                    <form className='shadow-md rounded md:px-8 px-4 pt-6 pb-8 mb-4' onSubmit={isEdit ? updateHandler : submitHandler}>
                         <label className="block text-gray-700 text-sm font-bold mb-6" htmlFor="username">
                             <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" placeholder="What do you want to buy" value={name} onChange={e => setName(e.target.value)} />
                         </label>
@@ -119,23 +121,23 @@ const ShoppingList = () => {
 
             </div>
 
-            <div className='border-l-2 border-blue-600 p-4 md:w-3/5'>
+            <div className='md:border-l-2 border-blue-600 p-4 md:w-3/5'>
                 <ul className='min-h-48'>
                     {shoppingList.length > 0 ? shoppingList.map((item, index) => (
-                        <li key={index} className={item.isChecked ? 'line-through flex justify-between border-b-2 border-blue-400 p-3' : 'flex justify-between border-b-2 border-blue-400 p-3'}>
+                        <li key={index} className={item.isChecked ? 'line-through flex justify-between border-b-2 border-blue-400 md:p-3 p-2' : 'flex justify-between border-b-2 border-blue-400 md:p-3 p-2'}>
                             <input type="checkbox" name="" id="" onChange={(e) => checkboxHandler(e, index)} />
                             <span className='px-4 w-3/5'>{item.name}</span>
-                            <span className='w-1/6'>${item.price}</span>
+                            <span className='w-1/6 mr-3'>${item.price}</span>
                             <div>
-                                <button className='mr-4 cursor-pointer hover:text-blue-800' title='Edit' onClick={() => editIconHandler(index)}><FaEdit /></button>
+                                <button className='md:mr-4 mr-2 cursor-pointer hover:text-blue-800' title='Edit' onClick={() => editIconHandler(index)}><FaEdit /></button>
                                 <button className='cursor-pointer hover:text-red-800' title='Delete' onClick={() => deleteHandler(index)}><FaTrash /></button>
                             </div>
 
                         </li>
                     )) : <h2 className='pb-32 text-3xl text-red-800'>No list at the moment</h2>}
                 </ul>
-                <div className='border shadow-lg bg-gray-300 mt-10 h-16 py-4 px-6'>
-                    <p className='flex justify-between'><span>Total: ${totalPrice}</span><span>Spent: ${checkedPrice}</span><span>Balance: ${uncheckedPrice}</span></p>
+                <div className='border shadow-lg bg-gray-300 mt-10 h-16 lg:py-4 lg:px-6 py-2 px-2 '>
+                    <p className='flex justify-between text-center'><span className='mr-2'>Total: ${totalPrice}</span><span className='mr-2'>Spent: ${checkedPrice}</span><span>Balance: ${uncheckedPrice}</span></p>
                 </div>
             </div>
         </div>
