356d52f84cc175c27363316a3b82bf3fa9cdad96
Stellamaris Nwafor <wendumaris@gmail.com>
2022-11-13 23:32:46 +0100

    worked on responsiveness

--- src/components/ShoppingList.jsx
+++ src/components/ShoppingList.jsx
@@ -59,21 +59,21 @@ const ShoppingList = () => {
 
     const submitHandler = (e) => {
         e.preventDefault();
-        if ((name != '') && (+price > 0)) {
+        if ((name != '') && (+price > 0 && +price < 1000000)) {
             setShoppingList((prev) => [...prev, { name: name, price: price, isChecked: false }]);
+            setName('');
+            setPrice(0);
         }
         updateTotalPrice();
     }
 
     const updateHandler = (e) => {
         e.preventDefault();
-        if ((name != '') && (+price > 0)) {
+        if ((name != '') && (+price > 0 && +price < 1000000)) {
             const updateListCopy = [...shoppingList];
             updateListCopy[currentId].name = name;
             updateListCopy[currentId].price = price;
             setShoppingList(updateListCopy);
-            setName('');
-            setPrice(0);
             setIsEdit(false);
         }
     }
@@ -96,22 +96,25 @@ const ShoppingList = () => {
     }
 
     const deleteHandler = (id) => {
-        const newShoppingList = shoppingList.filter(item => shoppingList.indexOf(item) != id);
-        setShoppingList(newShoppingList);
+        if (confirm('oops!! 😱😱\nAre you sure you want to delete ??')) {
+            const newShoppingList = shoppingList.filter(item => shoppingList.indexOf(item) != id);
+            setShoppingList(newShoppingList);
+        }
+
     }
 
 
     return (
-        <div className='sm:p-0 bg-white mt-4 text-black py-5 md:px-8 h-auto md:flex justify-between box-border'>
+        <div className='bg-white mt-4 text-black py-5 lg:px-8 h-auto lg:flex lg:justify-between box-border'>
             <div className='p-4'>
                 <h1 className='md:text-4xl text-blue-600 font-bold text-center text-2xl '>Shopping App 🛒</h1>
-                <div className='md:w-96 w-full md:max-W-xs mt-4'>
+                <div className='md:w-96 w-full md:max-W-xs mt-4 mx-auto'>
                     <form className='shadow-md rounded md:px-8 px-4 pt-6 pb-8 mb-4' onSubmit={isEdit ? updateHandler : submitHandler}>
                         <label className="block text-gray-700 text-sm font-bold mb-6" htmlFor="username">
                             <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" placeholder="What do you want to buy" value={name} onChange={e => setName(e.target.value)} />
                         </label>
                         <label className="block text-gray-700 text-sm font-bold mb-6" htmlFor="username">
-                            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="number" placeholder="Price" value={price} onChange={e => setPrice(e.target.value)} />
+                            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="number" placeholder="Price" value={price} onChange={e => setPrice(e.target.value.slice(0, 6))} />
                         </label>
                         <button className="block cursor-pointer bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
                             {isEdit ? 'Edit Item' : 'Add Item'}
@@ -121,14 +124,14 @@ const ShoppingList = () => {
 
             </div>
 
-            <div className='md:border-l-2 border-blue-600 p-4 md:w-3/5'>
+            <div className='border-t-2 lg:border-l-2  border-blue-600 p-4 md:w-3/5 mx-auto'>
                 <ul className='min-h-48'>
                     {shoppingList.length > 0 ? shoppingList.map((item, index) => (
                         <li key={index} className={item.isChecked ? 'line-through flex justify-between border-b-2 border-blue-400 md:p-3 p-2' : 'flex justify-between border-b-2 border-blue-400 md:p-3 p-2'}>
                             <input type="checkbox" name="" id="" onChange={(e) => checkboxHandler(e, index)} />
-                            <span className='px-4 w-3/5'>{item.name}</span>
-                            <span className='w-1/6 mr-3'>${item.price}</span>
-                            <div>
+                            <span className='px-4 w-3/6 lg:w-3/5'>{item.name}</span>
+                            <span className='w-1/7 mr-2'>${item.price}</span>
+                            <div className='w-auto'>
                                 <button className='md:mr-4 mr-2 cursor-pointer hover:text-blue-800' title='Edit' onClick={() => editIconHandler(index)}><FaEdit /></button>
                                 <button className='cursor-pointer hover:text-red-800' title='Delete' onClick={() => deleteHandler(index)}><FaTrash /></button>
                             </div>
@@ -136,8 +139,8 @@ const ShoppingList = () => {
                         </li>
                     )) : <h2 className='pb-32 text-3xl text-red-800'>No list at the moment</h2>}
                 </ul>
-                <div className='border shadow-lg bg-gray-300 mt-10 h-16 lg:py-4 lg:px-6 py-2 px-2 '>
-                    <p className='flex justify-between text-center'><span className='mr-2'>Total: ${totalPrice}</span><span className='mr-2'>Spent: ${checkedPrice}</span><span>Balance: ${uncheckedPrice}</span></p>
+                <div className='border shadow-lg text-white mt-10 h-18 lg:py-4 lg:px-6 py-4 px-2 bg-blue-600 rounded-lg '>
+                    <p className='flex justify-between text-center'><span className='mr-2'>Total: ${totalPrice}</span><span className='mr-2'>Spent: ${checkedPrice}</span><span>Bal: ${uncheckedPrice}</span></p>
                 </div>
             </div>
         </div>
